5bdd23beb7a1967beba1d9d376c1db7ddad34531
Darcy Ye <darcyye@silverhand.io>
2024-11-08 11:07:17 +0800

    fix: api integration tests

--- packages/integration-tests/src/tests/api/application/application.secrets.test.ts
+++ packages/integration-tests/src/tests/api/application/application.secrets.test.ts
@@ -30,18 +30,17 @@ describe('application secrets', () => {
   it.each(Object.values(ApplicationType))(
     'should or not to create application secret for %s applications per type',
     async (type) => {
-      const application = await createApplication(
-        'application',
-        type,
-        cond(
+      const application = await createApplication('application', type, {
+        ...cond(
           type === ApplicationType.Protected && {
             protectedAppMetadata: {
               origin: 'https://example.com',
               subDomain: randomString(),
             },
           }
-        )
-      );
+        ),
+        ...cond(type === ApplicationType.SAML && { isThirdParty: true }),
+      });
       expect(application.secret).toMatch(new RegExp(`^${internalPrefix}`));
 
       // Check the default secret
